ecd5c3568a9c8ae336cd94d3be19f276410aff77
johni1228 <akirus1228@gmail.com>
2021-07-29 23:32:17 -0700

    chore: upload service - add vaildatoruser func

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService : UsersService) {} // instance and inject
+
+    async vaildatorUser(username: string, pwd: string ): Promise<any> {
+        const user = await this.usersService.findOne(username);
+        if (user && user.password == pwd ) {
+            const { password, ...result } = user;
+            return result    // return user without password, in other word, username and userId
+        }
+        return null;
+    }
+    
+}
