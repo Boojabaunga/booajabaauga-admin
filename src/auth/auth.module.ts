2065a5edffa8f06473b8e9064bb73c90bf880b0e
johni1228 <akirus1228@gmail.com>
2021-07-29 23:35:09 -0700

    chore: upload auth.module - import userModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [UsersModule],    // import UsersModule in order to use user.service in auth module
   providers: [AuthService]
 })
 export class AuthModule {}
